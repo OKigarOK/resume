b5e5e07cb7739787f4b1b856b2f6328d9d96287e
OKigarOK <91433816+OKigarOK@users.noreply.github.com>
2022-09-26 12:23:37 +0300

    init

---
+++ translate.js
@@ -0,0 +1,114 @@
+const ru = {
+    name: 'Игорь Карзюк',
+    description_item1: 'В далекие 90-е у меня появился первый компьютер ПК «Байт» (копия ZX Spectrum). Радости не было предела, игры быстро надоели, и я решил изучать Basic. Как же это было круто: что-то делаешь и практически сразу можно увидеть результат. Но, язык высокого уровня не позволял делать что-то необычное, красивое и быстрое. Решил перейти к изучению Assembler, возможно, это и стало роковой ошибкой. Копание в низкоуровневом языке угнетало, горы кода чтобы сделать что-то простое, компиляция и … всё «падает». Тёмная сторона мной овладела, решил переключится, вернуться к спорту, готовится к поступлению...',
+    description_item2: 'И вот, спустя много лет я уже взрослый дядька, муж, отец двух детей. Человек, прошедший путь от охранника до директора, столкнулся с тем что моё нынешнее положение (а именно работа) не приносит ни удовольствия, ни удовлетворения. Как-то под новый год у нас «крякнули» рабочие сервера, довелось плотно поработать с разными ребятами из IT. Начал изучать смотреть, общаться с людьми. Меня как будто снова окрылило. Неужели существует то, чего мне так не хватало тогда, много лет назад, что мне действительно интересно, где развитие и совершенство не знает границ. Решение принято - стать крутым Фронтендером намерен я.',
+    description_item3: 'Не всё даётся легко и просто, но кое какой бэкграунд здорово помогает не останавливаться и идти дальше. Вперед и только вперёд…',
+    contacts_sidebar: 'Контакты:',
+    networks_sidebar: 'Социальные сети:',
+    interests_head: 'Сферы интересов:',
+    interests_item1: 'Программирование',
+    interests_item2: 'Английский язык',
+    interests_item3: 'Блоггинг',
+    interests_item4: 'Психотерапия',
+    study_head: 'Образование и обучение',
+    study_item1: 'Июнь 2022 - Июль 2022: Продвинутый JavaScript (Лев Гуржиханов, Frontend TeamLead)',
+    study_item2: 'Январь 2022 - Июль 2022: Последний Фронтенд Марафон (Артем Димитров, Frontend TeamLead)',
+    study_item3: 'Октябрь 2021 - Январь 2022: Разработка веб-сайтов с использованием HTML, CSS и JavaScript (Образовательный центр программирования и высоких технологий IT-Academy)',
+    study_item4: 'Октябрь 2020 - Ноябрь 2021: Теория и практика психологического консультирования (Психолог. Преподаватель психологии высшей квалификационной категории Л.В.Клишевич)',
+    study_item5: '1998-2004: Белорусская государственная академия авиации. Высшее образование. Кафедра технической эксплуатации радиоэлектронного оборудования. Радио-инженер',
+    work_head: 'Опыт работы:',
+    job_title: 'Frontend-разработчик (неоплачиваемая стажировка 10.2021-08.2022)',
+    responsibilities: 'Должностные обязанности:',
+    responsibilities_item1: 'Создание учебных проектов, веб-приложение на Javascript с использованием React и современных интерфейсных инструментов',
+    responsibilities_item2: 'Создание готовых макетов (HTML + SCC, Figma)',
+    responsibilities_item3: 'Работа со сторонними библиотеками',
+    responsibilities_item4: 'Работа с асинхронными запросами (fetch, WebSocket, REST API)',
+    responsibilities_item5: 'Участие в кодревью, рефакторинг',
+    achievements: 'Достижения:',
+    achievements_item1: 'Освоил новые технологии',
+    achievements_item2: 'Принимал архитектурные решения на проектах',
+    achievements_item3: 'Внёс вклад в улучшение платформы',
+    contact_head: 'Связаться со мной:',
+    contact_name: 'Ваше имя:',
+    contact_email: 'Ваш E-mail:',
+    contact_message: 'Ваше сообщение:',
+    send_btn: 'Отправить',
+
+    change_to_EN: 'EN',
+    change_to_RU: 'РУ',
+}
+const en = {
+    name: 'Igor Karzyuk',
+    description_item1: 'In the distant 90s, I got my first computer PC "Byte" (a copy of the ZX Spectrum). There was no limit to the joy, the games quickly got tired, and I decided to study Basic. How cool it was: you do something and almost immediately you can see the result "But, the high-level language did not allow to do something unusual, beautiful and fast. I decided to move on to learning Assembler, perhaps this was a fatal mistake. Digging in a low-level language was depressing, mountains of code to do something simple, compiling and ... everything “falls.” The dark side took possession of me, I decided to switch, to return to sports, I am preparing for admission ...',
+    description_item2: 'And now, many years later, I am already an adult uncle, husband, father of two children. A person who has gone from a security guard to a director is faced with the fact that my current position (namely, work) brings neither pleasure nor satisfaction. Somehow, before the new year, our working servers “grunted”, I had a chance to work closely with different guys from IT. Began to study watch, communicate with people. It was as if I was engulfed again. Is there really something that I lacked so much then, many years ago, that I really wonder where development and perfection knows no bounds. The decision has been made - I intend to become a cool Frontender.',
+    description_item3: 'Not everything is easy and simple, but some background helps a lot not to stop and move on. Forward and only forward…',
+    contacts_sidebar: 'Contacts:',
+    networks_sidebar: 'Social networks:',
+    interests_head: 'Fields of interests:',
+    interests_item1: 'Programming',
+    interests_item2: 'Learning English',
+    interests_item3: 'Blogging',
+    interests_item4: 'Psychotherapy',
+    study_head: 'Education and training:',
+    study_item1: 'June 2022 - July 2022: Advanced JavaScript (Lev Gurzhikhanov, Frontend TeamLead)',
+    study_item2: 'January 2022 - July 2022: Last Frontend Marathon (Artem Dimitrov, Frontend TeamLead)',
+    study_item3: 'October 2021 - January 2022: Website development with HTML, CSS and JavaScript (IT-Academy Educational Center for Programming and High Tech)',
+    study_item4: 'October 2020 - November 2021: Theory and practice of psychological counseling (Psychologist. Teacher of psychology of the highest qualification category L.V. Klishevich)',
+    study_item5: '1998-2004: Belarusian State Aviation Academy. Higher education. Department of technical operation of radio-electronic equipment. radio engineer',
+    work_head: 'Work experience:',
+    job_title: 'Frontend developer (unpaid internship 10.2021-08.2022)',
+    responsibilities: 'Job responsibilities:',
+    responsibilities_item1: 'Creating educational projects, Javascript web application using React and modern Front-End tools',
+    responsibilities_item2: 'Creation of ready-made layouts (HTML + SCC, Figma)',
+    responsibilities_item3: 'Working with third party libraries',
+    responsibilities_item4: 'Working with asynchronous requests (fetch, WebSocket, REST API)',
+    responsibilities_item5: 'Participation in code review, refactoring',
+    achievements: 'Achievements:',
+    achievements_item1: 'Mastered new technologies',
+    achievements_item2: 'Made architectural decisions on projects',
+    achievements_item3: 'Contributed to improving the platform',
+    contact_head: 'Contact with me:',
+    contact_name: 'Your name:',
+    contact_email: 'Your E-mail:',
+    contact_message: 'Your message:',
+    send_btn: 'Send',
+
+    change_to_EN: 'EN',
+    change_to_RU: 'RU',
+}
+
+let currentLang = ru;
+translate();
+
+function translate() {
+    const elementsToTranslate = document.querySelectorAll('[my-translate]')
+
+    for (let element of elementsToTranslate) {
+        const key = element.getAttribute('my-translate');
+        element.textContent = currentLang[key];
+    }
+}
+
+function changeToRu() {
+    currentLang = ru;
+    setRuIcon()
+    translate();
+}
+
+function changeToEn() {
+    currentLang = en;
+    setEnIcon()
+    translate();
+}
+
+function setEnIcon() {
+    const element = document.querySelector('.set_lang')
+    element.firstElementChild.classList.add('icon_opacity')
+    element.lastElementChild.classList.remove('icon_opacity')
+}
+
+function setRuIcon() {
+    const element = document.querySelector('.set_lang')
+    element.firstElementChild.classList.remove('icon_opacity')
+    element.lastElementChild.classList.add('icon_opacity')
+}
